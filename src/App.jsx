7986293a8293f8aa366b10a4d917ff81c29bac74
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-02 16:15:32 -0500

    Added the basic HTML to the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,104 @@ import { Link, Route, Switch } from 'react-router-dom'
 
 export function App() {
   return (
-    <>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="home container">
+      <header className="header">
+        <h1>Job Tracker&trade;</h1>
       </header>
-      <Switch>
+
+      <aside className="notifications">
+        <h2>Notifications</h2>
+        <a href="#new">New</a>
+        <ul>
+          <li>
+            <h3>Follow up with PetCo</h3>
+            <p>You applied 3 days ago.</p>
+          </li>
+          <li>
+            <h3>Interview with ACME Inc.</h3>
+            <p>Monday at 2 pm</p>
+          </li>
+        </ul>
+      </aside>
+
+      <main className="companies">
+        <h2>Companies</h2>
+        <a href="#new">New</a>
+        <ul>
+          <li>
+            <h3>PetCo</h3>
+            <a href="https://pet.co">https://pet.co</a>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Veritatis eius dicta iure ullam.
+            </p>
+            <p>
+              <a href="#edit">Edit</a>
+            </p>
+          </li>
+          <li>
+            <h3>ACME Inc.</h3>
+            <a href="https://pet.co">https://pet.co</a>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae,
+              tenetur. Perferendis magnam maxime ipsam voluptas nesciunt illum.
+            </p>
+            <p>
+              <a href="#edit">Edit</a>
+            </p>
+          </li>
+          <li>
+            <h3>PetCo</h3>
+            <a href="https://pet.co">https://pet.co</a>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              ducimus totam voluptate ad.
+            </p>
+            <p>
+              <a href="#edit">Edit</a>
+            </p>
+          </li>
+          <li>
+            <h3>ACME Inc.</h3>
+            <a href="https://pet.co">https://pet.co</a>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+              porro perferendis. Velit corrupti suscipit blanditiis ipsa
+              dignissimos.
+            </p>
+            <p>
+              <a href="#edit">Edit</a>
+            </p>
+          </li>
+        </ul>
+      </main>
+
+      <aside className="interactions">
+        <h2>Interactions</h2>
+        <a href="#new">New</a>
+        <ul>
+          <li>
+            <h3>Emailed PetCo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>4 days ago</p>
+          </li>
+          <li>
+            <h3>Emailed ACME Inc.</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <p>2 days ago</p>
+          </li>
+          <li>
+            <h3>Applied at PetCo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>2 days ago</p>
+          </li>
+        </ul>
+      </aside>
+
+      <footer className="footer">
+        <p>&copy; Made with &hearts; by Mandy Wade in 2021</p>
+      </footer>
+      {/* <Switch>
         <Route exact path="/">
           Home
         </Route>
@@ -31,7 +111,7 @@ export function App() {
           Page 2
         </Route>
         <Route path="*">Not Found</Route>
-      </Switch>
-    </>
+      </Switch> */}
+    </div>
   )
 }
